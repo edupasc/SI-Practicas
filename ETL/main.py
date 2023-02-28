c4c8daf34cedd98a6a27d68ce59836da4b7dece6
edupasc <59749852+edupasc@users.noreply.github.com>
2023-02-28 18:25:23 +0100

    ejercicio2 completado

--- ETL/main.py
+++ ETL/main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import pandas as pd
 import json
 import sqlite3
@@ -21,13 +16,17 @@ def storeFilesInDB(conn):
     # create tables
     c.execute('''CREATE TABLE IF NOT EXISTS devices (id TEXT, ip TEXT, localizacion TEST, responsable_id TEXT, analisis_id INTEGER)''')
     c.execute('''CREATE TABLE IF NOT EXISTS responsable (nombre TEXT PRIMARY KEY, telefono TEXT, rol TEXT)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS analisis (id INTEGER PRIMARY_KEY, puertos_abiertos TEXT, servicios INTEGER, servicios_inseguros INTEGER, vulnerabilidades_detectadas INTEGER)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS analisis (id INTEGER PRIMARY_KEY, puertos_abiertos TEXT, no_puertos_abiertos INTEGER, servicios INTEGER, servicios_inseguros INTEGER, vulnerabilidades_detectadas INTEGER)''')
 
     for d in devices:
         responsable = d['responsable']
         c.execute("INSERT OR IGNORE INTO responsable VALUES (?, ?, ?)", (responsable['nombre'], responsable['telefono'], responsable['rol']))
         analisis = d['analisis']
-        c.execute("INSERT INTO analisis (puertos_abiertos, servicios, servicios_inseguros, vulnerabilidades_detectadas) VALUES (?, ?, ?, ?)", (json.dumps(analisis['puertos_abiertos']), analisis['servicios'], analisis['servicios_inseguros'], analisis['vulnerabilidades_detectadas']))
+        if analisis["puertos_abiertos"] == 'None':
+            ports = 0
+        else:
+            ports = len(analisis["puertos_abiertos"])
+        c.execute("INSERT INTO analisis (puertos_abiertos, no_puertos_abiertos, servicios, servicios_inseguros, vulnerabilidades_detectadas) VALUES (?, ?, ?, ?, ?)", (json.dumps(analisis['puertos_abiertos']), ports, analisis['servicios'], analisis['servicios_inseguros'], analisis['vulnerabilidades_detectadas']))
         analisis_id = c.lastrowid
         c.execute("INSERT INTO devices (id, ip, localizacion, responsable_id, analisis_id) VALUES (?, ?, ?, ?, ?)", (d['id'], d['ip'], d['localizacion'], responsable['nombre'], analisis_id))
 
@@ -36,20 +35,37 @@ def storeFilesInDB(conn):
 def showInfo(conn):
     devices = pd.read_sql_query("SELECT * from devices", conn)
     print("NÚMERO DE DISPOSITIVOS: " + str(len(devices.index)))
-    # alerts = pd.read_sql_query("SELECT * from alerts", conn)
+    #alerts = pd.read_sql_query("SELECT * from alerts", conn)
     # print("NÚMERO DE ALERTAS: " + str(len(alerts.index)))
     c = conn.cursor()
     no_alerts = c.execute("SELECT count(*) from alerts").fetchone()[0]
     print("NÚMERO DE ALERTAS: " + str(no_alerts))
+    analysis = pd.read_sql_query("SELECT * from analisis", conn)
+    mean = analysis['no_puertos_abiertos'].mean()
+    std = analysis['no_puertos_abiertos'].std()
+    print("NÚMERO MEDIO DE PUERTOS ABIERTOS: " + str(mean))
+    print("DESVIACIÓN ESTÁNDAR DEL NÚMERO DE PUERTOS ABIERTOS: " + str(std))
+    mean = analysis['servicios_inseguros'].mean()
+    std = analysis['servicios_inseguros'].std()
+    print("NÚMERO MEDIO DE SERVICIOS INSEGUROS DETECTADOS: " + str(mean))
+    print("DESVIACIÓN ESTÁNDAR DEL NÚMERO DE SERVICIOS INSEGUROS DETECTADOS: " + str(std))
+    mean = analysis['vulnerabilidades_detectadas'].mean()
+    std = analysis['vulnerabilidades_detectadas'].std()
+    print("NÚMERO MEDIO DE VULNERABILIDADES DETECTADAS : " + str(mean))
+    print("DESVIACIÓN ESTÁNDAR DEL NÚMERO DE VULNERABILIDADES DETECTADAS: " + str(std))
+    min = analysis["no_puertos_abiertos"].min()
+    max = analysis["no_puertos_abiertos"].max()
+    print("NÚMERO MÁXIMO DE PUERTOS ABIERTOS EN UN DISPOSITIVO: " + str(max))
+    print("NÚMERO MÍNIMO DE PUERTOS ABIERTOS EN UN DISPOSITIVO: " + str(min))
+    min = analysis["vulnerabilidades_detectadas"].min()
+    max = analysis["vulnerabilidades_detectadas"].max()
+    print("NÚMERO MÁXIMO DE VULNERABILIDADES DETECTADAS EN UN DISPOSITIVO: " + str(max))
+    print("NÚMERO MÍNIMO DE VULNERABILIDADES DETECTADAS EN UN DISPOSITIVO: " + str(min))
 
 
 
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     conn = sqlite3.connect("database.sqlite")
     # storeFilesInDB(conn)
     showInfo(conn)
     conn.close()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
