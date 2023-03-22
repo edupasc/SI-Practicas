8f6587027276325ab7609aff7609df5c3c9d9999
andresAmaya288 <100414051+andresAmaya288@users.noreply.github.com>
2023-03-22 17:00:33 +0100

    ahora ya si funciona :)

--- ETL/main.py
+++ ETL/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 import sqlite3
+import calendar
 
 def storeFilesInDB(conn):
     # alerts
@@ -16,7 +17,8 @@ def storeFilesInDB(conn):
     # create tables
     c.execute('''CREATE TABLE IF NOT EXISTS devices (id TEXT, ip TEXT, localizacion TEST, responsable_id TEXT, analisis_id INTEGER)''')
     c.execute('''CREATE TABLE IF NOT EXISTS responsable (nombre TEXT PRIMARY KEY, telefono TEXT, rol TEXT)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS analisis (id INTEGER PRIMARY_KEY, puertos_abiertos TEXT, no_puertos_abiertos INTEGER, servicios INTEGER, servicios_inseguros INTEGER, vulnerabilidades_detectadas INTEGER)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS analisis (id INTEGER PRIMARY_KEY, ip TEXT, puertos_abiertos TEXT, no_puertos_abiertos INTEGER, servicios INTEGER, servicios_inseguros INTEGER, vulnerabilidades_detectadas INTEGER)''')
+
 
     for d in devices:
         responsable = d['responsable']
@@ -26,10 +28,9 @@ def storeFilesInDB(conn):
             ports = 0
         else:
             ports = len(analisis["puertos_abiertos"])
-        c.execute("INSERT INTO analisis (puertos_abiertos, no_puertos_abiertos, servicios, servicios_inseguros, vulnerabilidades_detectadas) VALUES (?, ?, ?, ?, ?)", (json.dumps(analisis['puertos_abiertos']), ports, analisis['servicios'], analisis['servicios_inseguros'], analisis['vulnerabilidades_detectadas']))
+        c.execute("INSERT INTO analisis (ip, puertos_abiertos, no_puertos_abiertos, servicios, servicios_inseguros, vulnerabilidades_detectadas) VALUES (?, ?, ?, ?, ?, ?)", (d['ip'],json.dumps(analisis['puertos_abiertos']), ports, analisis['servicios'], analisis['servicios_inseguros'], analisis['vulnerabilidades_detectadas']))
         analisis_id = c.lastrowid
         c.execute("INSERT INTO devices (id, ip, localizacion, responsable_id, analisis_id) VALUES (?, ?, ?, ?, ?)", (d['id'], d['ip'], d['localizacion'], responsable['nombre'], analisis_id))
-
     conn.commit()
 
 def showInfo(conn):
@@ -63,32 +64,36 @@ def showInfo(conn):
     print("NÚMERO MÍNIMO DE VULNERABILIDADES DETECTADAS EN UN DISPOSITIVO: " + str(min))
 
 def infoPriority(conn):
-    alerts = pd.read_sql_query("SELECT * from alerts", conn)
-    analysis = pd.read_sql_query("SELECT * from analisis", conn)
-    devices = pd.read_sql_query("SELECT * from devices", conn)
-    c = conn.cursor()
-    alerts = alerts[['timestamp', 'prioridad', 'origen','destino']]
-    analysis = analysis[['vulnerabilidades_detectadas']]
-    df = pd.DataFrame()
-    priority = []
-    alertsByPriority = []
+   vulnerabilities = pd.read_sql_query("SELECT DISTINCT a.prioridad, v.vulnerabilidades_detectadas, a.row_num FROM analisis v  JOIN ( SELECT  prioridad, origen, destino, ROW_NUMBER() OVER (ORDER BY prioridad) AS row_num FROM alerts) a  ON v.ip = a.origen OR v.ip = a.destino;", conn)
+   for j in range(1, 4):
+       vulnByPriority = vulnerabilities[(vulnerabilities['prioridad'] == j)]
+       print("----------------------------------------------------------------------------------------")
+       print("Numero de entradas prioridad " + str(j) + ": " + str(len(vulnByPriority.index)))
+       print("Media de vulnerabilidades_detectadas con prioridad " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].mean()))
+       print("Mediana de vulnerabilidades_detectadas con prioridad " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].median()))
+       print("Varianza de vulnerabilidades_detectadas con prioridad " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].var()))
+       print("Min de vulnerabilidades_detectadas con prioridad " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].min()))
+       print("Máx de vulnerabilidades_detectadas con prioridad " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].max()))
 
-    for j in range(1,4):
-        df['priority ' + str(j)] = None
-        alertsByPriority.append([])
-        priority.append(alerts[(alerts['prioridad'] == j)])
-        for i in devices:
-            alertsByPriority.append(priority[j][(priority[j]['origen'] == i['ip']) | (priority[j]['destino'] == i['ip'])])
+def infoDate(conn):
+   vulnerabilities = pd.read_sql_query("SELECT DISTINCT a.timestamp, v.vulnerabilidades_detectadas, a.row_num FROM analisis v  JOIN ( SELECT  x.timestamp ,origen, destino, ROW_NUMBER() OVER (ORDER BY prioridad) AS row_num FROM alerts x) a  ON v.ip = a.origen OR v.ip = a.destino;", conn)
+   vulnerabilities['timestamp'] = pd.to_datetime(vulnerabilities['timestamp'])
 
-
-
-
-
+   for j in range(7,9):
+       vulnByPriority = vulnerabilities[(vulnerabilities['timestamp'].dt.month == j)]
+       print("----------------------------------------------------------------------------------------")
+       print("Numero de entradas del mes " + str(j) + ": " + str(len(vulnByPriority.index)))
+       print("Media de vulnerabilidades_detectadas del mes " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].mean()))
+       print("Mediana de vulnerabilidades_detectadas del mes " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].median()))
+       print("Varianza de vulnerabilidades_detectadas del mes " + str(j)+ ": " + str(vulnByPriority["vulnerabilidades_detectadas"].var()))
+       print("Min de vulnerabilidades_detectadas del mes " + str(j)+ ": " + str(vulnByPriority["vulnerabilidades_detectadas"].min()))
+       print("Máx de vulnerabilidades_detectadas del mes " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].max()))
 
 
 if __name__ == '__main__':
     conn = sqlite3.connect("database.sqlite")
-    # storeFilesInDB(conn)
+    #storeFilesInDB(conn)
     showInfo(conn)
     infoPriority(conn)
+    infoDate(conn)
     conn.close()
