68cb5588f37337ea8797f047e9dc0d888884a54a
andresAmaya288 <100414051+andresAmaya288@users.noreply.github.com>
2023-03-23 16:11:25 +0100

    añadidos valores ausentes

--- ETL/main.py
+++ ETL/main.py
@@ -65,8 +65,11 @@ def showInfo(conn):
 
 def infoPriority(conn):
    vulnerabilities = pd.read_sql_query("SELECT DISTINCT a.prioridad, v.vulnerabilidades_detectadas, a.row_num FROM analisis v  JOIN ( SELECT  prioridad, origen, destino, ROW_NUMBER() OVER (ORDER BY prioridad) AS row_num FROM alerts) a  ON v.ip = a.origen OR v.ip = a.destino;", conn)
+   alerts = pd.read_sql_query("SELECT * from alerts", conn)
+   missingValues = len(alerts.index)
    for j in range(1, 4):
        vulnByPriority = vulnerabilities[(vulnerabilities['prioridad'] == j)]
+       missingValues -= len(vulnByPriority.index)
        print("----------------------------------------------------------------------------------------")
        print("Numero de entradas prioridad " + str(j) + ": " + str(len(vulnByPriority.index)))
        print("Media de vulnerabilidades_detectadas con prioridad " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].mean()))
@@ -75,13 +78,18 @@ def infoPriority(conn):
        print("Min de vulnerabilidades_detectadas con prioridad " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].min()))
        print("Máx de vulnerabilidades_detectadas con prioridad " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].max()))
 
+   print("----------------------------------------------------------------------------------------")
+   print("Valores ausentes: " + str(missingValues))
 def infoDate(conn):
    vulnerabilities = pd.read_sql_query("SELECT DISTINCT a.timestamp, v.vulnerabilidades_detectadas, a.row_num FROM analisis v  JOIN ( SELECT  x.timestamp ,origen, destino, ROW_NUMBER() OVER (ORDER BY prioridad) AS row_num FROM alerts x) a  ON v.ip = a.origen OR v.ip = a.destino;", conn)
    vulnerabilities['timestamp'] = pd.to_datetime(vulnerabilities['timestamp'])
+   alerts = pd.read_sql_query("SELECT * from alerts", conn)
+   missingValues = len(alerts.index)
 
    for j in range(7,9):
        vulnByPriority = vulnerabilities[(vulnerabilities['timestamp'].dt.month == j)]
        print("----------------------------------------------------------------------------------------")
+       missingValues -= len(vulnByPriority.index)
        print("Numero de entradas del mes " + str(j) + ": " + str(len(vulnByPriority.index)))
        print("Media de vulnerabilidades_detectadas del mes " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].mean()))
        print("Mediana de vulnerabilidades_detectadas del mes " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].median()))
@@ -89,6 +97,10 @@ def infoDate(conn):
        print("Min de vulnerabilidades_detectadas del mes " + str(j)+ ": " + str(vulnByPriority["vulnerabilidades_detectadas"].min()))
        print("Máx de vulnerabilidades_detectadas del mes " + str(j) + ": " + str(vulnByPriority["vulnerabilidades_detectadas"].max()))
 
+
+
+   print("----------------------------------------------------------------------------------------")
+   print("Valores ausentes: " + str(missingValues))
 
 if __name__ == '__main__':
     conn = sqlite3.connect("database.sqlite")
